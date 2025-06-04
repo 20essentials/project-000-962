a1f89f074b102a5d83690c8aa171ffb4cfcd2486
DominicDev <s0l0estudi4r@gmail.com>
2025-06-04 17:59:40 +0200

    v0.0.1

---
+++ src/utils/consts.js
@@ -0,0 +1,80 @@
+export const INITIAL_HTML = `<main class="bg">
+  <h1></h1>
+</main>`;
+
+export const INITIAL_CSS = `*,
+*::after,
+*::before {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.bg {
+  display: flex;
+  min-height: 100dvh;
+  background-color: black;
+  background-image: url('https://20essentials.github.io/project-000-128/assets/n1.avif');
+  background-position: center;
+  background-size: cover;
+
+  h1 {
+    z-index: 9999;
+    mix-blend-mode: overlay;
+    inset: 0;
+    position: absolute;
+    animation: moveTitle 10s ease infinite alternate;
+  }
+}
+
+@keyframes moveTitle {
+  0% {
+    background-color: #f0f;
+  }
+
+  25% {
+    background-color: #0f0;
+  }
+
+  50% {
+    background-color: #00f;
+  }
+
+  75% {
+    background-color: #f00;
+  }
+
+  100% {
+    background-color: #ff0;
+  }
+}`;
+
+export const INITIAL_JS = `function generateStars() {
+  stars = Array.from({ length: STAR_COUNT }, () => ({
+    x: Math.random() * width,
+    y: Math.random() * height,
+    z: Math.random() * MAX_DEPTH
+  }));
+}
+
+function drawStars() {
+  ctx.fillStyle = '#0001';
+  ctx.fillRect(0, 0, width, height);
+
+  for (const star of stars) {
+    star.z -= SPEED;
+    if (star.z <= 0) star.z = MAX_DEPTH;
+
+    const scale = MAX_DEPTH / star.z;
+    const x = (star.x - halfWidth) * scale + halfWidth;
+    const y = (star.y - halfHeight) * scale + halfHeight;
+    const dist = Math.hypot(x - halfWidth, y - halfHeight);
+
+    ctx.beginPath();
+    ctx.arc(x, y, scale, 0, Math.PI * 2);
+    ctx.fillStyle = red;
+    ctx.fill();
+  }
+
+  requestAnimationFrame(drawStars);
+}`;
