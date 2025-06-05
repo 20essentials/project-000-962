24cf2936a82a52718ec4ee5ea0a137fed87ca6ad
DominicDev <s0l0estudi4r@gmail.com>
2025-06-05 16:08:25 +0200

    v0.0.9

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,5 +1,6 @@
 export const $ = (el: string) => document.querySelector(el);
 export const $$ = (el: string) => document.querySelectorAll(el);
+import { encode } from "js-base64";
 
 export function baseUrl(path: string) {
   return new URL(path.replace(/^\/+/, ''), import.meta.env.SITE).toString();
@@ -11,4 +12,37 @@ export function debounce(func: (...args: any[]) => void, msWait: number) {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), msWait);
   };
+}
+
+export function updateUrlQueries({
+  htmlValue,
+  cssValue,
+  jsValue
+}: {
+  htmlValue: string;
+  cssValue: string;
+  jsValue: string;
+}) {
+  const languageToUrl = [
+    ['HTML', htmlValue],
+    ['CSS', cssValue],
+    ['JavaScript', jsValue]
+  ];
+
+  const languagesWithValue = languageToUrl.filter(([_, languageValue]) => {
+    if (languageValue !== '') return true;
+    if (languageValue.length > 1) return true;
+    return false;
+  });
+
+  const url = new URL(window.location.href);
+  const urlParams = url.searchParams;
+
+  languagesWithValue.forEach(([languageName, languageValue]) => {
+    urlParams.set(languageName, encode(languageValue));
+  });
+  const [, hashedCode] = url.href.split('/?');
+  if (hashedCode) {
+    window.history.pushState(null, '', `/?${hashedCode}`);
+  }
 }
