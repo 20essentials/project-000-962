d5ca340a46dbd4e968e95ad3cba9844b4fc8a8e7
DominicDev <s0l0estudi4r@gmail.com>
2025-06-05 15:10:22 +0200

    v0.0.8

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,5 +2,13 @@ export const $ = (el: string) => document.querySelector(el);
 export const $$ = (el: string) => document.querySelectorAll(el);
 
 export function baseUrl(path: string) {
-  return new URL(path.replace(/^\/+/, ""), import.meta.env.SITE).toString();
+  return new URL(path.replace(/^\/+/, ''), import.meta.env.SITE).toString();
+}
+
+export function debounce(func: (...args: any[]) => void, msWait: number) {
+  let timeout: ReturnType<typeof setTimeout>;
+  return (...args: any[]) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), msWait);
+  };
 }
