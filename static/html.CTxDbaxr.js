7478d8861a7125251a32622c6681f7325ee12cb1
DominicDev <s0l0estudi4r@gmail.com>
2025-06-13 22:25:37 +0200

    Updates

--- static/html.CTxDbaxr.js
+++ static/html.CTxDbaxr.js
@@ -1,4 +1,4 @@
-import{m as s}from"./Hero.astro_astro_type_script_index_0_lang.zAIFV9Qc.js";import"./functions.CwmkLS0h.js";/*!-----------------------------------------------------------------------------
+import{m as s}from"./Hero.astro_astro_type_script_index_0_lang.CQ3Wld7g.js";import"./functions.CwmkLS0h.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
